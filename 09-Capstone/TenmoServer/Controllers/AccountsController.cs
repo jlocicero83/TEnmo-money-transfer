27d622a9fe6b1aa92a439ee32e43d8c35bb76c67
Dan Goepfert <dangoepfert@gmail.com>
2020-06-30 15:52:25 -0400

    began implementing AccounsController

--- 09-Capstone/TenmoServer/Controllers/AccountsController.cs
+++ 09-Capstone/TenmoServer/Controllers/AccountsController.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using TenmoServer.DAO;
-using TenmoServer.Security;
 
 namespace TenmoServer.Controllers
 {
     [Route("[controller]")]
+    [Authorize]
     [ApiController]
     public class AccountsController : ControllerBase
     {
+        private readonly IAccountDAO accountDAO;
+        public AccountsController(IAccountDAO accountDAO)
+        {
+            this.accountDAO = accountDAO;
+        }
 
+        [HttpGet("{id}")]
+        public ActionResult<decimal> GetBalance()
+        {
+            decimal result;
+            result = accountDAO.GetAccountBalance(Convert.ToInt32(User.Identity));
+            return result;
+        }
 
-        
     }
 }
