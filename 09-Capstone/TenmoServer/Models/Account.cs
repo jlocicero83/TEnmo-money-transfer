00ae3a3dc64b505c2157a06f4b6efeb7af2713fc
Joe LoCicero <jlocicero83@gmail.com>
2020-06-30 10:14:23 -0400

    Added Models

---
+++ 09-Capstone/TenmoServer/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public int UserID { get; set; }
+        public decimal Balance { get; set; }
+
+
+    }
+}
---
+++ 09-Capstone/TenmoServer/Models/Transfers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Transfers
+    {
+        public int TransferID { get; set; }
+        public int TransferTypeID { get; set; }
+        public int TransferStatusID { get; set; }
+        public int FromAccountID { get; set; }
+        public int ToAccountID { get; set; }
+        public decimal TransferAmount { get; set; }
+
+
+
+
+    }
+}
